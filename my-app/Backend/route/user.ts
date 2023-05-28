19bea980371a319cc535188818fc37b9626302a1
Mohamedaziz1919 <guerfalamohamed2001@gmail.com>
2023-05-28 18:52:55 +0200

    admin dashboard done

--- my-app/Backend/route/user.ts
+++ my-app/Backend/route/user.ts
@@ -1,10 +1,11 @@
 const express = require ("express")
 const router =express.Router()
-import {getUser, userLogin ,signupUser }  from '../controller/userController';
+import {getUser, userLogin ,signupUser,deleteUser }  from '../controller/userController';
 
 router.get("/getall", getUser);
 router.post("/login",userLogin);
 router.post("/signup",signupUser)
+router.delete("/deleted/:id",deleteUser)
 
 
 export default router 
---
+++ my-app/Client/src/components/AdminCheck.vue
@@ -0,0 +1,105 @@
+<template>
+<div>
+    <navbar/>
+  <!-- <div class="card" style="width: 18rem;">
+    <div class="card-body" style="justify-content: center; align-items: center;">
+      <h5 class="card-title">username </h5>
+      <h6 class="card-subtitle mb-2 text-muted"> useremail </h6>
+    </div>
+  </div> -->
+  <div>
+<h6>.</h6>
+    </div>
+  <div id="aa">
+    <div> 
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td> {{user.id}} </td>
+        <td> {{user.firstname}} </td>
+        <td> {{user.lastname}} </td>
+        <td> {{user.email}} </td>
+        <button style="background-color: red ; color:white "  @click="handledelete(user.id)" >Delete</button>
+      </tr>
+
+    </table>
+      </div>
+  </div>
+  </div>
+</template>
+<script>
+import navbar from "./navbar.vue";
+import axios from "axios";
+export default {
+  components: { 
+    navbar
+  },
+  data() {
+    return {
+      users: { 
+        id:'',
+        firstname:'',
+        lastname:'',
+        email: '',
+        password:''
+      }
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      fetch("http://localhost:3000/auth/getAll")
+        .then(response => response.json())
+        .then(user => {
+          this.users = user; 
+        })
+        .catch(error => {
+          console.error("Error fetching users:", error);
+        });
+    },
+    handledelete(id){
+       if(window.confirm("are you sure that u want to delete it")){
+    axios.delete(`http://localhost:3000/auth/deleted/${id}`)
+    .then((res)=> {
+          alert(" user deleted"); 
+          this.fetchUsers();
+        })
+    .catch((err)=>{console.log(err)})
+  }
+
+}
+}
+};
+</script>
+
+<style>
+/* CSS Styles */
+#aa{
+    margin-top: 300px;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+  /* margin-top: 200px; */
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+tr:nth-child(even) {
+  background-color: #f9f9f9;
+}
+</style>
